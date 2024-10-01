3ca2b84b17851df7fa1d46f189ead7b68048f9d0
plutonium.239.811 <plutonium.239.811@gmail.com>
2024-10-01 21:19:53 +0000

    check what is receieved

--- functions/api/upload.ts
+++ functions/api/upload.ts
@@ -1,4 +1,12 @@
+import type { PlotlyDataLayoutConfig } from "plotly.js-dist-min";
 import { verifyCLIToken, type Env } from "./utils";
+
+export type PlotFormData = {
+    plot_data: PlotlyDataLayoutConfig,
+    name: string,
+    timestamp: number,
+    time: string
+}
 
 export const onRequestPost: PagesFunction<Env> = async (context) => {
 
@@ -6,10 +14,17 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
     const res = await verifyCLIToken(request, context.env)
     if (res instanceof Response) return res
     
-    const data = request.formData
-    console.log("received data:", context.data);
+    const data = await request.formData()
+    // console.log("received data:", JSON.stringify(context.data));
     console.log("received form data:", data);
+    console.log("received form data:", JSON.stringify(data));
 
+    for (const [key, value] of data.entries()) {
+        console.log(`${key}: ${value}`);
+    }
+
+    
+    // console.log("received plot keys:", data as PlotFormData);
 
     // const userData: SingleUserData = await makeAPIfetch(
     //     makeRESTdocURL(context.env, "userdata", decryptedjwt.user_id), 
